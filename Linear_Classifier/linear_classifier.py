5dd4ea663383fd5ae789f6faab36caf9fc632d26
Chad Paik <cpaik1004@gmail.com>
2017-03-21 19:13:52 -0400

    Add SVM Classifier

--- Linear_Classifier/linear_classifier.py
+++ Linear_Classifier/linear_classifier.py
@@ -8,7 +8,7 @@ class LinearClassifier:
 
         self.W = std * np.random.randn(num_dim*num_classes)
         self.W = self.W.reshape((num_dim,num_classes))
-        
+
     def softmax_loss(self, X, y, reg_str):
         loss = 0.0
         dW = np.zeros_like(self.W)
@@ -31,25 +31,57 @@ class LinearClassifier:
 
         dW = (X.T).dot(dscores)
         dW += reg_str * self.W
-        
+
         return loss, dW
 
-    def train(self, X, y, lr, reg_str, num_iters):
+    def svm_loss(self, X, y, reg):
+        loss = 0.0
+        dW = np.zeros(self.W.shape)
+        num_classes = self.W.shape[1]
+        num_train = X.shape[0]
+        scores = np.dot(X, self.W)
+        correctScores = scores[np.arange(num_train),y]
+
+        matrix = scores - correctScores.reshape((num_train,1))+1
+        matrix[np.arange(num_train),y] = 0
+        margins = np.maximum(0,matrix)
+        loss = np.sum(margins)
+
+        dWB = margins
+        dWB[margins > 0] = 1# all the indices with greater than zero has been replaced with 1
+        #dwt[margins < 0] = 0
+
+        incorrectCounts = np.sum(dWB, axis=1)
+        dWB[np.arange(num_train),y] = -incorrectCounts
+        dW = X.T.dot(dWB)
+        loss /= num_train
+        loss += 0.5*reg*np.sum(self.W * self.W)
+
+        dW /= num_train
+        dW += reg*(self.W)
+
+        return loss, dW
+
+    def train(self, X, y, lr, reg_str, num_iters, type='SVM'):
 
         num_train = X.shape[0]
         batch_size = 35
-        
+
         for iteration in range(num_iters):
             batch_idx = np.random.choice(num_train, batch_size)
             X_batch = X[batch_idx]
             y_batch = y[batch_idx]
-            
-            loss, dW = self.softmax_loss(X_batch, y_batch, reg_str)
-            self.W = self.W - lr * dW
+
+            if (type == 'SVM'):
+                loss, dW = self.svm_loss(X_batch, y_batch, reg_str)
+            else:
+                loss, dW = self.softmax_loss(X_batch, y_batch, reg_str)
+
+            self.W += -lr * dW
 
             if (iteration % 500 == 0):
                 print('Loss at iteration %d/%d: %f' % (iteration, num_iters, loss))
-            
+
     def predict(self, X):
         scores = np.dot(X, self.W)
         y_pred = np.argmax(scores, axis=1)
